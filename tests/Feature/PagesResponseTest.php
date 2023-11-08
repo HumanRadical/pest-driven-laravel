2247a93a71732f9f975d37f0ed80db4473b45af7
Easton Meth <easton.meth@gmail.com>
2023-11-08 17:50:45 -0500

    Rename and fix response test

--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use function Pest\Laravel\get;
 
+uses(RefreshDatabase::class);
+
 it('returns a successful response for home page', function () {
-    // Act & Assert
     get(route('home'))
         ->assertOk();
 });
