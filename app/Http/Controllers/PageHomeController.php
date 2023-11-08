afa4f34aec56314e4c6de66d5a545a4da6aeea28
Easton Meth <easton.meth@gmail.com>
2023-11-08 17:39:57 -0500

    Only render released courses

--- app/Http/Controllers/PageHomeController.php
+++ app/Http/Controllers/PageHomeController.php
@@ -9,8 +9,10 @@ class PageHomeController extends Controller
 {
     public function __invoke()
     {
-        $courses = Course::all();
-        
+        $courses = Course::query()
+            ->whereNotNull('released_at')
+            ->get();
+
         return view('home', compact('courses'));
     }
 }
