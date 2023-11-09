b88578a286da57622709a1c5cbd888cdf1b8a8e2
Easton Meth <easton.meth@gmail.com>
2023-11-09 13:50:01 -0500

    Refactor course overview test

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -10,19 +10,13 @@ uses(RefreshDatabase::class);
 
 it('shows courses overview', function () {
     // Arrange
-    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A', 'released_at' => Carbon::now()]);
-    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B', 'released_at' => Carbon::now()]);
-    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C', 'released_at' => Carbon::now()]);
+    $courses = Course::factory(3)->released()->create();
 
     // Act & Assert
     get(route('home'))
         ->assertSeeText([
-            'Course A',
-            'Description Course A',
-            'Course B',
-            'Description Course B',
-            'Course C',
-            'Description Course C',
+            ...$courses->pluck('title')->toArray(),
+            ...$courses->pluck('description')->toArray(),
         ]);
 });
 
