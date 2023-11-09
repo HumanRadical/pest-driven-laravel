b6365a2498052f5814bb456ec1023d6670320c84
Easton Meth <easton.meth@gmail.com>
2023-11-09 14:28:47 -0500

    pass course into details controller

--- app/Http/Controllers/PageCourseDetailsController.php
+++ app/Http/Controllers/PageCourseDetailsController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class PageCourseDetailsController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Course $course)
     {
         return view('course-details');
     }
