71f203fac8b610f996e09ccbc988e1670fd3c54b
Easton Meth <easton.meth@gmail.com>
2023-11-09 13:38:27 -0500

    Add comments

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -9,10 +9,12 @@ use function Pest\Laravel\get;
 uses(RefreshDatabase::class);
 
 it('shows courses overview', function () {
+    // Arrange
     Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A', 'released_at' => Carbon::now()]);
     Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B', 'released_at' => Carbon::now()]);
     Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C', 'released_at' => Carbon::now()]);
 
+    // Act & Assert
     get(route('home'))
         ->assertSeeText([
             'Course A',
@@ -25,18 +27,22 @@ it('shows courses overview', function () {
 });
 
 it('shows only released courses', function () {
+    // Arrange
     Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
     Course::factory()->create(['title' => 'Course B']);
 
+    // Act & Assert
     get(route('home'))
         ->assertSeeText('Course A')
         ->assertDontSeeText('Course B');
 });
 
 it('shows courses by release date', function () {
+    // Arrange
     Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
     Course::factory()->create(['title' => 'Course B', 'released_at' => Carbon::now()]);
 
+    // Act & Assert
     get(route('home'))
         ->assertSeeTextInOrder([
             'Course B',
--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -7,6 +7,7 @@ use function Pest\Laravel\get;
 uses(RefreshDatabase::class);
 
 it('returns a successful response for home page', function () {
+    // Act & Assert
     get(route('home'))
         ->assertOk();
 });
