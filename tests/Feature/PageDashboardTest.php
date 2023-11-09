97a91a136dcab0899f3475a0c3d0512efb48c3db
Easton Meth <easton.meth@gmail.com>
2023-11-09 16:10:28 -0500

    add tests

--- tests/Feature/PageDashboardTest.php
+++ tests/Feature/PageDashboardTest.php
@@ -7,3 +7,11 @@ it('lists purchased courses', function () {
 it('does not list unpurchased courses', function () {
     //expect()->
 });
+
+it('shows latest purchased course first', function () {
+    //expect()->
+});
+
+it('includes link to product videos', function () {
+    //expect()->
+});
