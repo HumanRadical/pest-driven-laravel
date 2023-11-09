d49c4f58ab549e3a63bc4ef12145ded1d7f8f070
Easton Meth <easton.meth@gmail.com>
2023-11-09 15:08:20 -0500

    add course and video relationships to models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -14,6 +14,11 @@ class Course extends Model
         'learnings' => 'array'
     ];
 
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+
     public function scopeReleased(Builder $query): Builder
     {
         return $query->whereNotNull('released_at');
