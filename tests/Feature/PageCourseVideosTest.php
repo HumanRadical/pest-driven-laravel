b560c96cf8756b5e00b1bd3d6f4a87f974293ef5
Easton Meth <easton.meth@gmail.com>
2023-11-10 17:06:50 -0500

    update tests

--- tests/Feature/PageCourseVideosTest.php
+++ tests/Feature/PageCourseVideosTest.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Models\Course;
+
+use function Pest\Laravel\get;
+
 it('cannot be accessed by guest', function () {
     // Arrange
+    $course = Course::factory()->create();
+
+    // Act & Assert
+    get(route('page.course-videos', $course))
+        ->assertRedirect(route('login'));
 });
 
 it('includes video player', function () {
-    //expect()->
+    // Arrange
+    loginAsUser();
+    $course = Course::factory()->create();
+
+    // Act & Assert
+    get(route('page.course-videos', $course))
+        ->assertOk()
+        ->assertSeeLivewire(VideoPlayer::class);
 });
 
 it('shows first course video by default', function () {
