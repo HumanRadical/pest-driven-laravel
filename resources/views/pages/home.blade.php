4c2ef13c3f241b9856f787e39040aec511d0c563
Easton Meth <easton.meth@gmail.com>
2023-11-10 16:24:09 -0500

    Show logout if logged in

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,13 @@
-<a href="{{ route('login') }}">Login</a>
+@guest
+    <a href="{{ route('login') }}">Login</a>
+@else
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button>Logout</button>
+    </form>
+@endguest
+
+
 @foreach($courses as $course)
     <h2>{{ $course->title }}</h2>
     <p>{{ $course->description }}</p>
