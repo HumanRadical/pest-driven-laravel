2d0ec91e60ce7a7c444783bd4d4c28e4e3f59ca4
Easton Meth <easton.meth@gmail.com>
2023-11-10 16:49:16 -0500

    Create course videos test file

---
+++ tests/Feature/PageCourseVideosTest.php
@@ -0,0 +1,17 @@
+<?php
+
+it('cannot be accessed by guest', function () {
+    // Arrange
+});
+
+it('includes video player', function () {
+    //expect()->
+});
+
+it('shows first course video by default', function () {
+    //expect()->
+});
+
+it('shows provided course video', function () {
+    //expect()->
+});
