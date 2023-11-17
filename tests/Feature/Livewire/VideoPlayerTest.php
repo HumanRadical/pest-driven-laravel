87b2f799943647ffd30ce74572fefe62b4da6ecd
Easton Meth <easton.meth@gmail.com>
2023-11-17 17:40:46 -0500

    Flesh out test

--- tests/Feature/Livewire/VideoPlayerTest.php
+++ tests/Feature/Livewire/VideoPlayerTest.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Livewire\VideoPlayer;
+use App\Models\Course;
+use App\Models\Video;
+use Livewire\Livewire;
+
 it('shows details for given video', function () {
-    //expect()->
+    // Arrange
+    $course = Course::factory()
+        ->has(Video::factory()->state([
+            'title' => 'Video title',
+            'description' => 'Video description',
+            'duration' => 10,
+        ]))->create();
+
+    // Act & Assert
+    Livewire::test(VideoPlayer::class, ['video' => $course->videos->first()])
+        ->assertSeeText([
+            'Video title',
+            'Video description',
+            '10min',
+        ]);
 });
 
 it('shows given video', function () {
