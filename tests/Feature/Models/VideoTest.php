1b856d11cad5a7c1dc8e88f71419cc544c92a0f0
Easton Meth <easton.meth@gmail.com>
2023-11-20 13:01:08 -0500

    ADd course test to Video model test

--- tests/Feature/Models/VideoTest.php
+++ tests/Feature/Models/VideoTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Course;
 use App\Models\Video;
 
 it('gives back readable video duration', function () {
@@ -10,3 +11,12 @@ it('gives back readable video duration', function () {
     expect($video->getReadableDuration())->toEqual('10min');
 });
 
+it('has courses', function () {
+    // Arrange
+    $video = Video::factory()
+        ->has(Course::factory())
+        ->create();
+
+    // Act & Assert
+    expect($video->course)->toBeInstanceOf(Course::class);
+});
