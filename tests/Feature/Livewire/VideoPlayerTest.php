7aee4907f725d48bc838c30a716193d952e9a8b5
Easton Meth <easton.meth@gmail.com>
2023-11-20 12:50:44 -0500

    Create video list test

--- tests/Feature/Livewire/VideoPlayerTest.php
+++ tests/Feature/Livewire/VideoPlayerTest.php
@@ -32,3 +32,23 @@ it('shows given video', function () {
     Livewire::test(VideoPlayer::class, ['video' => $video])
         ->assertSeeHtml('<iframe src="https://player.vimeo.com/video/' . $video->vimeo_id . '"');
 });
+
+it('shows list of all course videos', function () {
+    // Arrange
+    $course = Course::factory()
+        ->has(Video::factory(3)->state(new Sequence(
+            ['title' => 'First video'],
+            ['title' => 'Second video'],
+            ['title' => 'Third video'],
+        )))
+        ->create();
+
+    // Act & Assert
+    Livewire::test(VideoPlayer::class, ['video' => $course->videos()->first()])
+        ->assertSee([
+            'First video',
+            'Second video',
+            'Third video',
+        ]);
+});
+
