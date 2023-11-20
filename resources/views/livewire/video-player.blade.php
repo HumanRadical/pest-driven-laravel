ca17cb46409421a10d576ce65710c96ac4d9c031
Easton Meth <easton.meth@gmail.com>
2023-11-20 12:57:09 -0500

    Add root div

--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -1,10 +1,12 @@
+
 <div>
     <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_id }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     <h3>{{ $video->title }}  {{ $video->durationInMins }}min</h3>
     <p>{{ $video->description }}</p>
+    
+    <ul>
+        @foreach ($courseVideos as $courseVideo)
+            <li>{{ $courseVideo->title }}</li>
+        @endforeach
+    </ul>
 </div>
-<ul>
-    @foreach ($courseVideos as $courseVideo)
-        <li>{{ $courseVideo->title }}</li>
-    @endforeach
-</ul>
