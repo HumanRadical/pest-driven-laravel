3bfb63caf81ed721142b72ef5481aea2fcdde309
Easton Meth <easton.meth@gmail.com>
2023-11-21 14:40:19 -0500

    add and setup paddle

--- resources/views/pages/course-details.blade.php
+++ resources/views/pages/course-details.blade.php
@@ -1,3 +1,8 @@
+<script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+<script type="text/javascript">
+    Paddle.Setup({ vendor: {{ config('services.paddle.vendor-id') }} })
+</script>
+
 <h2>{{ $course->title }}</h2>
 <h3>{{ $course->tagline }}</h3>
 <p>{{ $course->description }}</p>
@@ -9,3 +14,4 @@
     @endforeach
 </ul>
 <img src="{{ asset("images/{$course->image}") }}" alt="Image for {{ $course->title }}">
+<a href="#!" class="paddle_button" data-product="{{ $course->paddle_product_id }}">Buy Now!</a>
