4c86cbf47d6872146ce1231582cb2331b728f115
Easton Meth <easton.meth@gmail.com>
2023-11-21 14:03:51 -0500

    Seed database with purchased courses

--- database/seeders/AddLocalTestUserSeeder.php
+++ database/seeders/AddLocalTestUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +13,14 @@ class AddLocalTestUserSeeder extends Seeder
     public function run(): void
     {
         if (App::environment() === 'local') {
-            User::create([
+            $user = User::create([
                 'email' => 'test@test.at',
                 'name' => 'Testie',
                 'password' => bcrypt('test'),
             ]);
+
+            $courses = Course::all();
+            $user->purchasedCourses()->attach($courses);
         }
     }
 }
