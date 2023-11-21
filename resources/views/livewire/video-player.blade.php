4fd09f893efde4531633d59ed395135e641133b5
Easton Meth <easton.meth@gmail.com>
2023-11-21 14:12:31 -0500

    Add course route

--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -1,7 +1,7 @@
 
 <div>
     <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_id }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-    <h3>{{ $video->title }}  {{ $video->durationInMins }}min</h3>
+    <h3>{{ $video->title }}  {{ $video->duration_in_mins }}min</h3>
     <p>{{ $video->description }}</p>
 
     @if ($video->alreadyWatchedByCurrentUser())
@@ -12,12 +12,16 @@
     
     <ul>
         @foreach ($courseVideos as $courseVideo)
-            @if ($this->isCurrentVideo($courseVideo))
-                {{ $courseVideo->title }}
-            @else 
-                <a href="{{ route('pages.course-videos', $courseVideo) }}"></a>
-                <li>{{ $courseVideo->title }}</li>
-            @endif
+            <li>
+                @if ($this->isCurrentVideo($courseVideo))
+                    {{ $courseVideo->title }}
+                @else 
+                    <a href="{{ route('pages.course-videos', [
+                        'course' => $courseVideo->course,
+                        'video' => $courseVideo,
+                    ]) }}">{{ $courseVideo->title }}</a>
+                @endif
+            </li>
         @endforeach
     </ul>
 </div>
